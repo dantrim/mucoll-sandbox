14f16263182d5ca275e394f2dcef61d02000b1f4
Beren <bozek3@uic.edu>
2021-08-16 23:17:05 -0500

    defined new histogram for plotting residual

--- analysis/cpp/src/make-histograms-loop.cpp
+++ analysis/cpp/src/make-histograms-loop.cpp
@@ -60,34 +60,65 @@ int main(int argc, char* argv[]) {
     tree->Add(input_file.c_str());
     std::cout << "Loaded TTree \"" << tree_name << "\" with " << tree->GetEntries() << " events" << std::endl;
 
-    // primary vertex related variables
+    /*
+      Here we declare local variables that will be used to hold the data
+      for each of the variables we wish to read from the input
+      TTree file. There must be one locally declared variable for
+      each of the variables in the TTree that we wish to read.
+      */
+
+
+    // reco-level primary vertex related variables
     float pv_xpos = 0;
     float pv_ypos = 0;
     float pv_zpos = 0;
 
-    // variables for mc particles
-    int* mcpdg = new int[5000000]; // ROOT is dumb so just allocate a ton of memory for these arrays
-    float* mc_px = new float[5000000];
-    float* mc_py = new float[5000000];
-    float* mc_xpos = 0;
-    int n_mc_particles_in_event;
+
+    // truth-level primary vertex proxies (we take the vertex positions from the truth muons from the muon gun as a proxy)
+     float truth_pv_xpos = 0;
+     float truth_pv_ypos = 0;
+     float truth_pv_zpos = 0;
+
+     // mc particle related variables
+     const size_t mc_particle_array_size = 5000000;
+     int* mcpdg = new int[mc_particle_array_size]; // pdg id
+     int* mcgst = new int[mc_particle_array_size]; // MC generator status (1 == generated particle, 0 = decay ??)
+     float* mc_px = new float[mc_particle_array_size]; // x momentum
+     float* mc_py = new float[mc_particle_array_size]; // y momentum
+     float* mc_vtx_xpos = new float[mc_particle_array_size]; // x coordinate of MC particle vertex
+     float* mc_vtx_ypos = new float[mc_particle_array_size]; // y coordinate of MC particle vertex
+     float* mc_vtx_zpos = new float[mc_particle_array_size]; // z coordinate of MC particle vertex
+     int n_mc_particles_in_event = 0; // the number of MC particles in the event
+     float mu_vtx_x_pos = 0;
+     float antimu_vtx_x_pos = 0;
 
     // attach the local variables to those in the TTree
+    tree->SetBranchAddress("nmcp" , &n_mc_particles_in_event);
     tree->SetBranchAddress("vtxxx", &pv_xpos);
-    tree->SetBranchAddress("mcvtx", &pv_xpos);
-    // tree->SetBranchAddress("vtyyy", &pv_ypos);
-    // tree->SetBranchAddress("vtzzz", &pv_zpos);
-    // tree->SetBranchAddress("mcpdg", mcpdg);
-    // tree->SetBranchAddress("nmcp", &n_mc_particles_in_event);
-    // tree->SetBranchAddress("mcmox", mc_px);
-    // tree->SetBranchAddress("mcmoy", mc_py);
+    tree->SetBranchAddress("mcvtx", mc_vtx_xpos);
+    //tree->SetBranchAddress("nmcp", &n_mc_particles_in_event); // we need to load the "nmcp" varialbe so that we know how many MC particles there are in an event
+    //tree->SetBranchAddress("vtyyy", &pv_ypos);
+    //tree->SetBranchAddress("vtzzz", &pv_zpos);
+    tree->SetBranchAddress("mcpdg", mcpdg);
+    tree->SetBranchAddress("mcgst", mcgst);
+    //tree->SetBranchAddress("mcmox", mc_px);
+    //tree->SetBranchAddress("mcmoy", mc_py);
 
     // instantiate the histograms that we want to make
     std::unique_ptr<TH1F> h_vtxxx = std::make_unique<TH1F>("h_vtxxx", "Reco PV x-position;PV x-position [#mum];Entries/bin", 50, -5, 5);
     std::unique_ptr<TCanvas> c_vtxxx = std::make_unique<TCanvas>("c_vtxxx");
-    std::unique_ptr<TH1F> h_mcvtx = std::make_unique<TH1F>("h_mcvtx", "MC PDG Id.;PDG Id.;Entries/bin", 50, -5, 5);
-    std::unique_ptr<TCanvas> c_mcvtx = std::make_unique<TCanvas>("c_mcvtx");
 
+    std::unique_ptr<TH1F> h_mcvtx_muon = std::make_unique<TH1F>("h_mcvtx_muon", "Muon Truth-level Vertex x-position;Vertex x-position [#mum];Entries/bin", 50, -5, 5);
+    std::unique_ptr<TCanvas> c_mcvtx_muon = std::make_unique<TCanvas>("c_mcvtx_muon");
+
+    std::unique_ptr<TH1F> h_mu_mcvtx_diff = std::make_unique<TH1F>("h_mu_mcvtx_diff", "Difference between Truth-level Muon and Anti-Muon Production Vertices;#Delta Vertex x-position [#mum];Entries/bin", 20, -0.2, 0.2);
+    std::unique_ptr<TCanvas> c_mu_mcvtx_diff = std::make_unique<TCanvas>("c_mu_mcvtx_diff");
+
+    std::unique_ptr<TH1F> h_mc_muon_count = std::make_unique<TH1F>("h_mc_muon_count", "Number of MC (anti-)muons per event;Number of (anti-)muons;Entries/bin", 10, -5, 5);
+    std::unique_ptr<TCanvas> c_mc_muon_count = std::make_unique<TCanvas>("c_mc_muon_count");
+
+    std::unique_ptr<TH1F> h_pvxresidual = std::make_unique<TH1F>("h_pvxresidual", "PV X Residual;Residual[#mum];Entries/bin", 50, -2.5, 2.5);
+    std::unique_ptr<TCanvas> c_pvxresidual = std::make_unique<TCanvas>("c_pvxresidual");
     // std::unique_ptr<TH1F> h_vtyyy = std::make_unique<TH1F>("h_vtyyy", "Reco PV y-position;PV y-position [#mum];Entries/bin", 50, -5, 5);
     // std::unique_ptr<TCanvas> c_vtyyy = std::make_unique<TCanvas>("c_vtyyy");
     // std::unique_ptr<TH1F> h_vtzzz = std::make_unique<TH1F>("h_vtzzz", "Reco PV z-position;PV z-position [mm];Entries/bin", 50, -10, 10);
@@ -115,31 +146,93 @@ int main(int argc, char* argv[]) {
         // h_vtzzz->Fill(pv_zpos * 1e3);
 
         // loop over the MC particles
-        // for(size_t iparticle = 0; iparticle < n_mc_particles_in_event; ++iparticle) {
-        //     int pdgid = mcpdg[iparticle];
+        float mu_vtx_x_pos = 0;
+        float antimu_vtx_x_pos = 0;
+        float pvxresidual = 0;
+        unsigned n_muons_in_event = 0;
+         for(size_t iparticle = 0; iparticle < n_mc_particles_in_event; ++iparticle) {
+             int pdgid = mcpdg[iparticle];
+             int generator_status = mcgst[iparticle];
 
-        //     // skip things we don't care about
-        //     if(std::abs(pdgid) > 2000) continue;
+             // skip things we don't care about (they are hadrons/baryons/etc)
+             if(std::abs(pdgid) > 2000) continue;
 
-        //     h_mcpdg->Fill(pdgid);
+             // let's only grab the muons with generator status == 1
 
-        //     // MC particle momentum of this particle
-        //     double px = mc_px[iparticle];
-        //     double py = mc_px[iparticle];
-        //     double transverse_momentum = sqrt(px*px + py*py); // GeV
-        //     if(transverse_momentum > 1.0) {
-        //         h_mcpdg_ptcut->Fill(pdgid);
-        //     }
-        // } // loop over iparticle
-    } // loop over ievent
 
-    // draw the histograms
-    c_vtxxx->cd();
-    h_vtxxx->Draw("hist");
-    h_vtxxx->SetLineColor(kBlack);
-    h_vtxxx->SetLineWidth(2);
-    c_vtxxx->SetLogy(do_logy);
-    c_vtxxx->SaveAs("h_vtxxx_none.png");
+             if(std::abs(pdgid) == 13 && generator_status == 1) {
+                    if (pdgid == 13){
+                     mu_vtx_x_pos = mc_vtx_xpos[iparticle] * 1e6;
+                     h_mcvtx_muon->Fill(mu_vtx_x_pos);
+                     h_mc_muon_count->Fill(1.5);
+
+                 } 
+                    else if(pdgid == -13) {
+                     antimu_vtx_x_pos = mc_vtx_xpos[iparticle] * 1e6;
+                     h_mc_muon_count->Fill(-1.5);
+                 }
+             }
+
+             //h_mcpdg->Fill(pdgid);
+
+             //// MC particle momentum of this particle
+             //double px = mc_px[iparticle];
+             //double py = mc_px[iparticle];
+             //double transverse_momentum = sqrt(px*px + py*py); // GeV
+             //if(transverse_momentum > 1.0) {
+             //    h_mcpdg_ptcut->Fill(pdgid);
+             //}
+         } // loop over iparticle
+
+         //h_mc_muon_count->Fill(n_muons_in_event);
+         float delta_mu_vtx_x = mu_vtx_x_pos - antimu_vtx_x_pos;
+         h_mu_mcvtx_diff->Fill(delta_mu_vtx_x);
+         pvxresidual = (pv_xpos - mu_vtx_x_pos)*1e7;
+
+         // std::cout << "event " << ievent << "  residual = " << pvxresidual << std::endl;
+
+         h_pvxresidual->Fill(pvxresidual);
+
+         } // loop over ievent
+
+     std::cout << "AAA" ;
+
+     // draw the histograms
+     c_vtxxx->cd();
+     h_vtxxx->Draw("hist");
+     h_vtxxx->SetLineColor(kBlack);
+     h_vtxxx->SetLineWidth(2);
+     c_vtxxx->SetLogy(do_logy);
+     c_vtxxx->SaveAs("h_vtxxx_none.png");
+
+     c_mcvtx_muon->cd();
+     h_mcvtx_muon->Draw("hist");
+     h_mcvtx_muon->SetLineColor(kBlack);
+     h_mcvtx_muon->SetLineWidth(2);
+     c_mcvtx_muon->SetLogy(do_logy);
+     c_mcvtx_muon->SaveAs("h_mcvtx_muon.png");
+
+     c_mc_muon_count->cd();
+     h_mc_muon_count->Draw("hist");
+     h_mc_muon_count->SetLineColor(kBlack);
+     h_mc_muon_count->SetLineWidth(2);
+     c_mc_muon_count->SetLogy(do_logy);
+     c_mc_muon_count->SaveAs("h_mc_muon_count.png");
+
+     c_mu_mcvtx_diff->cd();
+     h_mu_mcvtx_diff->Draw("hist");
+     h_mu_mcvtx_diff->SetLineColor(kBlack);
+     h_mu_mcvtx_diff->SetLineWidth(2);
+     c_mu_mcvtx_diff->SetLogy(do_logy);
+     c_mu_mcvtx_diff->SaveAs("h_mu_mcvtx_diff.png");
+
+    
+     c_pvxresidual->cd();
+     h_pvxresidual->Draw("hist");
+     h_pvxresidual->SetLineColor(kBlack);
+     h_pvxresidual->SetLineWidth(2);
+     c_pvxresidual->SetLogy(do_logy);
+     c_pvxresidual->SaveAs("h_pvxresidual.png");
 
     // c_mcvtx->cd();
     // h_mcvtx->Draw("hist");
